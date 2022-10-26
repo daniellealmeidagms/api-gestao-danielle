c2b9e98a64d692f37d24164d1442d93d4acf8976
Danielle Almeida <daniellealmeidagms@gmail.com>
2022-10-26 11:47:12 -0300

    add matricula

--- api-gestao.sql
+++ api-gestao.sql
@@ -76,6 +76,7 @@ ALTER TABLE "alunos_necessidades" ADD FOREIGN KEY ("necessidades_id") REFERENCES
 CREATE TABLE "alunos_cursos" (
   "alunos_id" serial,
   "cursos_id" serial,
+  "matricula" bigint UNIQUE NOT NULL,
   PRIMARY KEY ("alunos_id", "cursos_id")
 );
 
