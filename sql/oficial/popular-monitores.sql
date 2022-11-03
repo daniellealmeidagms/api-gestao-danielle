e8f524687928a7c5b3e275cb45944ad97f911c7b
Danielle Almeida <daniellealmeidagms@gmail.com>
2022-11-03 11:45:51 -0300

    Refatoração

--- sql/oficial/popular-monitores.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO monitores ("cpfMonitor", "nomeMonitor", "emailMonitor", "telefoneMonitor", "horarioAtendimentoMonitor", "formacaoMonitor", "supervisor")
-	VALUES 
-	('12345678912', 'Joaquina', 'joaquina@yahoo.com.br', 62987635489, 'SEG, QUA, SEX 8:00 ÀS 12:00', 'Pedagogia', 2),
-	('79846513258', 'Frederico', 'fred@gmail.com', 62987687936, 'SEG, TER, QUA 13:00 ÀS 17:00', 'Psicologia', 4),
-	('98763158821', 'Isabel', 'isabel@gmail.com', 62789635489, 'TER, QUI 8:00 ÀS 12:00', 'História', 6),
-	('24814013019', 'Afonso', 'afonso@hotmail.com', 62789635489, 'SEG, QUA, SEX 13:00 ÀS 17:00', 'História', 8),
-	('70649430281', 'Raimundo', 'raimundo@gmail.com', 62988763489, 'SEG, QUA, QUI, SEX 18:00 ÀS 22:00', 'Letras', 2);
